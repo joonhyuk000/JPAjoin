fda2d437c7a49b4dda615efba44ef45a6422e23d
github wonjibkim <gihub kbg5174@naver.com>
2024-01-03 02:52:37 +0900

    2024-01-03 JPA JOIN 특강

--- src/main/java/kopo/poly/controller/NoticeJoinController.java
+++ src/main/java/kopo/poly/controller/NoticeJoinController.java
@@ -1,8 +1,10 @@
 package kopo.poly.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import kopo.poly.dto.NoticeDTO;
 import kopo.poly.service.INoticeJoinService;
+import kopo.poly.util.CmmUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -24,7 +26,7 @@ public class NoticeJoinController {
     private final INoticeJoinService noticeJoinService;
 
     /**
-     * 게시판 리스트 보여주기
+     * JoinColumn 게시판 리스트 보여주기
      * <p>
      * GetMapping(value = "notice/noticeListUsingJoinColumn") =>
      * GET방식을 통해 접속되는 URL이 notice/noticeList 경우 아래 함수를 실행함
@@ -60,7 +62,7 @@ public class NoticeJoinController {
 
 
     /**
-     * 게시판 리스트 보여주기
+     * NativeQuery 게시판 리스트 보여주기
      * <p>
      * GetMapping(value = "notice/noticeListUsingJoinColumn") =>
      * GET방식을 통해 접속되는 URL이 notice/noticeList 경우 아래 함수를 실행함
@@ -95,7 +97,114 @@ public class NoticeJoinController {
     }
 
 
+    /**
+     * JPQL 게시판 리스트 보여주기
+     * <p>
+     * GetMapping(value = "notice/noticeListUsingJoinColumn") =>
+     * GET방식을 통해 접속되는 URL이 notice/noticeList 경우 아래 함수를 실행함
+     */
+    @GetMapping(value = "noticeListUsingJPQL")
+    public String noticeListUsingJPQL(HttpSession session, ModelMap model)
+            throws Exception {
+
+        // 로그 찍기(추후 찍은 로그를 통해 이 함수에 접근했는지 파악하기 용이하다.)
+        log.info(this.getClass().getName() + ".noticeListUsingJPQL Start!");
+
+        // 로그인된 사용자 아이디는 Session에 저장함
+        // 교육용으로 아직 로그인을 구현하지 않았기 때문에 Session에 데이터를 저장하지 않았음
+        // 추후 로그인을 구현할 것으로 가정하고, 공지사항 리스트 출력하는 함수에서 로그인 한 것처럼 Session 값을 생성함
+        session.setAttribute("SESSION_USER_ID", "USER01");
+
+        // 공지사항 리스트 조회하기
+        // Java 8부터 제공되는 Optional 활용하여 NPE(Null Pointer Exception) 처리
+        List<NoticeDTO> rList = Optional.ofNullable(noticeJoinService.getNoticeListUsingJPQL())
+                .orElseGet(ArrayList::new);
+
+        // 조회된 리스트 결과값 넣어주기
+        model.addAttribute("rList", rList);
+
+        // 로그 찍기(추후 찍은 로그를 통해 이 함수 호출이 끝났는지 파악하기 용이하다.)
+        log.info(this.getClass().getName() + ".noticeListUsingJPQL End!");
+
+        // 함수 처리가 끝나고 보여줄 HTML (Thymeleaf) 파일명
+        // templates/notice/noticeList.html
+        return "notice/noticeListJoin";
+
+    }
 
 
+    /**
+     * Query DSL  게시판 리스트 보여주기
+     * <p>
+     * GetMapping(value = "notice/noticeListUsingJoinColumn") =>
+     * GET방식을 통해 접속되는 URL이 notice/noticeList 경우 아래 함수를 실행함
+     */
+    @GetMapping(value = "noticeListUsingQueryDSL")
+    public String noticeListUsingQueryDSL(HttpSession session, ModelMap model)
+            throws Exception {
+
+        // 로그 찍기(추후 찍은 로그를 통해 이 함수에 접근했는지 파악하기 용이하다.)
+        log.info(this.getClass().getName() + ".noticeListUsingQueryDSL Start!");
+
+        // 로그인된 사용자 아이디는 Session에 저장함
+        // 교육용으로 아직 로그인을 구현하지 않았기 때문에 Session에 데이터를 저장하지 않았음
+        // 추후 로그인을 구현할 것으로 가정하고, 공지사항 리스트 출력하는 함수에서 로그인 한 것처럼 Session 값을 생성함
+        session.setAttribute("SESSION_USER_ID", "USER01");
+
+        // 공지사항 리스트 조회하기
+        // Java 8부터 제공되는 Optional 활용하여 NPE(Null Pointer Exception) 처리
+        List<NoticeDTO> rList = Optional.ofNullable(noticeJoinService.getNoticeListForQueryDSL())
+                .orElseGet(ArrayList::new);
+
+        // 조회된 리스트 결과값 넣어주기
+        model.addAttribute("rList", rList);
+
+        // 로그 찍기(추후 찍은 로그를 통해 이 함수 호출이 끝났는지 파악하기 용이하다.)
+        log.info(this.getClass().getName() + ".noticeListUsingQueryDSL End!");
+
+        // 함수 처리가 끝나고 보여줄 HTML (Thymeleaf) 파일명
+        // templates/notice/noticeList.html
+        return "notice/noticeListJoin";
+
+    }
+
+
+
+
+    /**
+     *  Query DSL 게시판 상세보기
+     */
+    @GetMapping(value = "noticeInfoDsl")
+    public String noticeInfoDsl(HttpServletRequest request, ModelMap model) throws Exception {
+
+        log.info(this.getClass().getName() + ".noticeInfo Start!");
+
+        String nSeq = CmmUtil.nvl(request.getParameter("nSeq"), "0"); // 공지글번호(PK)
+
+        /*
+         * ####################################################################################
+         * 반드시, 값을 받았으면, 꼭 로그를 찍어서 값이 제대로 들어오는지 파악해야함 반드시 작성할 것
+         * ####################################################################################
+         */
+        log.info("nSeq : " + nSeq);
+
+        /*
+         * 값 전달은 반드시 DTO 객체를 이용해서 처리함 전달 받은 값을 DTO 객체에 넣는다.
+         */
+        NoticeDTO pDTO = NoticeDTO.builder().noticeSeq(Long.parseLong(nSeq)).build();
+
+        // 공지사항 상세정보 가져오기
+        // Java 8부터 제공되는 Optional 활용하여 NPE(Null Pointer Exception) 처리
+        NoticeDTO rDTO = Optional.ofNullable(noticeJoinService.getNoticeInfoForQueryDSL(pDTO, true))
+                .orElseGet(() -> NoticeDTO.builder().build());
+
+        // 조회된 리스트 결과값 넣어주기
+        model.addAttribute("rDTO", rDTO);
+
+
+        log.info(this.getClass().getName() + ".noticeInfo End!");
+
+        return "notice/noticeInfo";
+    }
 
 }
